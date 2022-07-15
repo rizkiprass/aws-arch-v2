600845d1b9d20e0495941648aae11f8aecad5e21
Rizki Prasetya <rizkiprass@gmail.com>
2022-07-15 16:32:54 +0700

    fix sg vpcid

--- aws-sg.tf
+++ aws-sg.tf
@@ -1,47 +1,47 @@
-variable "openvpn-sg-port" {
-  type = map(any)
-  default = {
-    "openvpn"   = 943
-    "openvpn2"  = 945
-    "openvpn3"  = 1194
-    "openvpn4"  = 443
-    "openvpn5"  = 22
-  }
-}
-
-resource "aws_security_group" "openvpn-sg" {
-  name        = format("%s-%s-openvpn-sg", var.Customer, var.environment)
-  description = format("%s-%s-openvpn-sg", var.Customer, var.environment)
-  vpc_id      = aws_vpc.vpc.id
-  dynamic "ingress" {
-    for_each = var.openvpn-sg-port
-    content {
-      from_port = ingress.value
-      to_port   = ingress.value
-      protocol  = "tcp"
-      cidr_blocks = [
-      "0.0.0.0/0"]
-      description = ingress.key
-    }
-  }
-
-  egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-    cidr_blocks = [
-    "0.0.0.0/0"]
-  }
-  tags = merge(local.common_tags, {
-    Name = format("%s-%s-openvpn-sg", var.Customer, var.environment),
-  })
-}
+#variable "openvpn-sg-port" {
+#  type = map(any)
+#  default = {
+#    "openvpn"   = 943
+#    "openvpn2"  = 945
+#    "openvpn3"  = 1194
+#    "openvpn4"  = 443
+#    "openvpn5"  = 22
+#  }
+#}
+#
+#resource "aws_security_group" "openvpn-sg" {
+#  name        = format("%s-%s-openvpn-sg", var.Customer, var.environment)
+#  description = format("%s-%s-openvpn-sg", var.Customer, var.environment)
+#  vpc_id      = aws_vpc.vpc.id
+#  dynamic "ingress" {
+#    for_each = var.openvpn-sg-port
+#    content {
+#      from_port = ingress.value
+#      to_port   = ingress.value
+#      protocol  = "tcp"
+#      cidr_blocks = [
+#      "0.0.0.0/0"]
+#      description = ingress.key
+#    }
+#  }
+#
+#  egress {
+#    from_port = 0
+#    to_port   = 0
+#    protocol  = "-1"
+#    cidr_blocks = [
+#    "0.0.0.0/0"]
+#  }
+#  tags = merge(local.common_tags, {
+#    Name = format("%s-%s-openvpn-sg", var.Customer, var.environment),
+#  })
+#}
 
 //Prod-App-sg
 resource "aws_security_group" "Prod-App-sg" {
   name = format("%s-%s-App-sg", var.Customer, var.environment)
   description = format("%s-%s-App-sg", var.Customer, var.environment)
-  vpc_id = aws_vpc.vpc.id
+  vpc_id = module.vpc.vpc_id
   ingress {
     from_port = 22
     to_port = 22
@@ -89,7 +89,7 @@ resource "aws_security_group" "Prod-App-sg" {
 resource "aws_security_group" "Dev-App-sg" {
   name = format("%s-dev-App-sg", var.Customer)
   description = format("%s-dev-App-sg", var.Customer)
-  vpc_id = aws_vpc.vpc.id
+  vpc_id = module.vpc.vpc_id
   ingress {
     from_port = 22
     to_port = 22
@@ -137,7 +137,7 @@ resource "aws_security_group" "Dev-App-sg" {
 resource "aws_security_group" "Prod-Data-sg" {
   name = format("%s-%s-Datadb-sg", var.Customer, var.environment)
   description = format("%s-%s-Datadb-sg", var.Customer, var.environment)
-  vpc_id = aws_vpc.vpc.id
+  vpc_id = module.vpc.vpc_id
   ingress {
     from_port = 3306
     to_port = 3306
@@ -178,7 +178,7 @@ resource "aws_security_group" "Prod-Data-sg" {
 resource "aws_security_group" "web-alb-sg" {
   name = format("%s-%s-web-alb-sg", var.Customer, var.environment)
   description = format("%s-%s-web-alb-sg", var.Customer, var.environment)
-  vpc_id = aws_vpc.vpc.id
+  vpc_id = module.vpc.vpc_id
   ingress {
     from_port = 80
     to_port = 80
