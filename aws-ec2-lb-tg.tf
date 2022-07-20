c3a0c9016c6d1fc109d42e44b2c481a8ede69d88
Rizki Prasetya <rizkiprass@gmail.com>
2022-07-20 15:48:37 +0700

    tg vpcid

--- aws-ec2-lb-tg.tf
+++ aws-ec2-lb-tg.tf
@@ -139,7 +139,7 @@ resource "aws_lb_target_group" "albtg-dev-app" {
   name     = format("%s-dev-albtg-dev-app", var.Customer)
   port     = 80
   protocol = "HTTP"
-  vpc_id   = aws_vpc.vpc.id
+  vpc_id   = module.vpc.vpc_id
   health_check {
     path                = "/"
     protocol            = "HTTP"
