ced78552f93da0776f69ebe87b85a99c5fd5e103
rizkiprass <rizkiprass@gmail.com>
2022-08-06 17:53:25 +0700

    add port jenkins in webmaster sg

--- aws-sg.tf
+++ aws-sg.tf
@@ -78,6 +78,15 @@ resource "aws_security_group" "Prod-App-sg" {
     description = "http"
   }
 
+  ingress {
+    from_port = 8080
+    to_port   = 8080
+    protocol  = "tcp"
+    cidr_blocks = [
+    "10.0.0.0/16"]
+    description = "jenkins-webmaster"
+  }
+
   egress {
     from_port = 0
     to_port   = 0
