6afff935fb9830b8e7c71f1bf68bd5bffefac381
rizkiprass <rizkiprass@gmail.com>
2022-08-06 14:03:03 +0700

    open all jenkins

--- aws-sg.tf
+++ aws-sg.tf
@@ -241,7 +241,7 @@ resource "aws_security_group" "Jenkins-App-sg" {
     to_port   = 8080
     protocol  = "tcp"
     cidr_blocks = [
-    "10.0.0.0/16"]
+    "0.0.0.0/0"]
     description = "web"
   }
 
