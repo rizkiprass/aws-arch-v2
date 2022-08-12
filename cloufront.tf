f2624168669203daecfe2534ed9276f14f0af099
Rizki Prasetya <rizkiprass@gmail.com>
2022-08-12 17:36:30 +0700

    add cloudfront

---
+++ cloufront.tf
@@ -0,0 +1,53 @@
+resource "aws_cloudfront_distribution" "cloudfront-1" {
+  comment         = "cloudfront-1"
+  aliases         = ["rp-server.site"]
+  enabled         = true
+  is_ipv6_enabled = true
+#  web_acl_id      = var.waf
+  logging_config {
+    bucket = "tempo-media-cloudfront-log.s3.amazonaws.com"
+    include_cookies = false
+  }
+  origin {
+     domain_name = aws_lb.web-alb.dns_name
+     origin_id   = aws_lb.web-alb.dns_name
+     custom_origin_config {
+       http_port              = 80
+       https_port             = 443
+       origin_protocol_policy = "https-only"
+       origin_ssl_protocols   = ["TLSv1.1"]
+     }
+   }
+  restrictions {
+      geo_restriction {
+      restriction_type = "none"
+      }
+    }
+  viewer_certificate {
+       acm_certificate_arn      = aws_acm_certificate.cert-rp.arn
+       ssl_support_method       = "sni-only"
+       minimum_protocol_version = "TLSv1.2_2021"
+    }
+ default_cache_behavior {
+     allowed_methods  = ["GET", "HEAD"]
+     cached_methods   = ["GET", "HEAD"]
+     target_origin_id = aws_lb.web-alb.dns_name
+     viewer_protocol_policy = "redirect-to-https"
+     compress               = true
+#   origin_request_policy_id = aws_cloudfront_origin_request_policy.origin-policy.id
+#    cache_policy_id          = aws_cloudfront_cache_policy.cache-policy-60s.id
+ }
+  comment = "test-desc"
+    price_class = "PriceClass_All"
+    enabled = true
+#  ordered_cache_behavior {
+#    path_pattern     = "/*"
+#    allowed_methods  = ["GET", "HEAD"]
+#    cached_methods   = ["GET", "HEAD"]
+#    target_origin_id =  var.origin_cloudfront
+#    viewer_protocol_policy = "redirect-to-https"
+#    compress               = true
+#    origin_request_policy_id = aws_cloudfront_origin_request_policy.origin-policy.id
+#    cache_policy_id          = aws_cloudfront_cache_policy.cache-policy-60s.id
+#   }
+}
--- install_nginx.sh
+++ install_nginx.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 sudo apt update
 sudo apt install nginx -y
 
