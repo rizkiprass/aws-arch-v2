6204bbd5eef9f99511a7e506078e0a373a899852
Rizki Prasetya <rizkiprass@gmail.com>
2022-08-19 15:17:45 +0700

    add s3

--- aws-s3.tf
+++ aws-s3.tf
@@ -1,11 +1,39 @@
-#module "s3_bucket" {
-#  source = "terraform-aws-modules/s3-bucket/aws"
-#
-#  bucket = "sandbox-s3-bucket-1122"
-#  acl    = "private"
-#
-#  versioning = {
-#    enabled = true
-#  }
-#
-#}
+####WAF S3
+resource "aws_s3_bucket" "waf-log" {
+  bucket = "sandbox-waf-log"
+
+  tags = merge(local.common_tags, {
+    Name        = format("%s-%s-waf-log", var.Customer, var.environment),
+  })
+}
+
+resource "aws_s3_bucket_acl" "waf-log-acl" {
+  bucket = aws_s3_bucket.alb-log.id
+  acl    = "private"
+}
+####CF S3
+resource "aws_s3_bucket" "cf-log" {
+  bucket = "sandbox-cloudfront-log"
+
+  tags = merge(local.common_tags, {
+    Name        = format("%s-%s-cloudfront-log", var.Customer, var.environment),
+  })
+}
+
+resource "aws_s3_bucket_acl" "cloudfront-log-acl" {
+  bucket = aws_s3_bucket.alb-log.id
+  acl    = "private"
+}
+####ALB S3
+resource "aws_s3_bucket" "alb-log" {
+  bucket = "sandbox-alb-log"
+
+  tags = merge(local.common_tags, {
+    Name        = format("%s-%s-alb-log", var.Customer, var.environment),
+  })
+}
+
+resource "aws_s3_bucket_acl" "alb-log-acl" {
+  bucket = aws_s3_bucket.alb-log.id
+  acl    = "private"
+}
