95dd283336c85b398c055b74ad85e21f89eeb451
Rizki Prasetya <rizkiprass@gmail.com>
2022-09-08 20:50:12 +0700

    fix sg web

--- aws-sg.tf
+++ aws-sg.tf
@@ -162,7 +162,7 @@ resource "aws_security_group" "web-sg" {
     "0.0.0.0/0"]
   }
   lifecycle {
-    create_before_destroy = true
+    create_before_destroy = false
   }
 
   tags = merge(local.common_tags, {
