fae0afba264b3551f473d7516200505b67e41973
rizkiprass <rizkiprass@gmail.com>
2022-10-29 16:27:27 +0700

    add route table endpoint.tf

--- endpoint.tf
+++ endpoint.tf
@@ -15,9 +15,10 @@ resource "aws_vpc_endpoint" "s3" {
 
 resource "aws_vpc_endpoint_route_table_association" "private_s3_1" {
   vpc_endpoint_id = aws_vpc_endpoint.s3.id
+  route_table_id  = module.vpc.private_route_table_ids[0]
+}
+
+resource "aws_vpc_endpoint_route_table_association" "private_s3_1" {
+  vpc_endpoint_id = aws_vpc_endpoint.s3.id
   route_table_id  = module.vpc.private_route_table_ids[1]
 }
-//resource "aws_vpc_endpoint_route_table_association" "private_s3_2" {
-//  vpc_endpoint_id = aws_vpc_endpoint.s3.id
-//  route_table_id  = aws_route_table.intra-rt.id
-//}
