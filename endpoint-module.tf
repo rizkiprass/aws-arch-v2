e113f41a729534211c7b517b2518407ffe4fb69b
rizkiprass <rizkiprass@gmail.com>
2022-10-29 16:11:11 +0700

    add endpoint

--- endpoint-module.tf
+++ endpoint-module.tf
@@ -2,7 +2,7 @@
 //  source = "modules/vpc-endpoints"
 //
 //  vpc_id             = module.vpc.vpc_id
-//  security_group_ids = [data.aws_security_group.default.id]
+//  security_group_ids = [data.aws_security_group]
 //
 //  endpoints = {
 //    s3 = {
@@ -12,9 +12,9 @@
 //  })
 //    }
 //  }
-
-
+//
+//
 //  data "aws_security_group" "default" {
-//  name   = "default"
+//  name   = "endpoint-sg"
 //  vpc_id = module.vpc.vpc_id
 //}
