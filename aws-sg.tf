20cb3a224646d7228f7ca6d88b9337b361e7d29c
rizkiprass <rizkiprass@gmail.com>
2022-10-29 13:57:20 +0700

    test-change-sg-bastion

--- aws-sg.tf
+++ aws-sg.tf
@@ -60,6 +60,16 @@ resource "aws_security_group" "bastion-host-sg" {
       description = ingress.key
     }
   }
+
+  ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    cidr_blocks = [
+    "10.0.0.0/16"]
+    description = "https"
+  }
+
   egress {
     from_port = 0
     to_port   = 0
