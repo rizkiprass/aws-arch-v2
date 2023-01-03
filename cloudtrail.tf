a5624db66b9a1ecd89913ff8b901266b496ef36f
Rizki Prasetya <rizkiprass@gmail.com>
2023-01-03 15:41:32 +0700

    ad cloudtrails

---
+++ cloudtrail.tf
@@ -0,0 +1,22 @@
+module "cloudtrail" {
+  source = "cloudposse/cloudtrail/aws"
+  # Cloud Posse recommends pinning every module to a specific version
+  # version     = "x.x.x"
+  namespace                     = "eg"
+  stage                         = "dev"
+  name                          = "sandbox-trails"
+  enable_log_file_validation    = true
+  include_global_service_events = true
+  is_multi_region_trail         = false
+  enable_logging                = true
+  s3_bucket_name                = module.cloudtrail_s3_bucket.bucket_id
+}
+
+module "cloudtrail_s3_bucket" {
+  source = "cloudposse/cloudtrail-s3-bucket/aws"
+  # Cloud Posse recommends pinning every module to a specific version
+  # version     = "x.x.x"
+  namespace = "eg"
+  stage     = "dev"
+  name      = "sandbox-management-trail"
+}
