aca9d0ead3691ce921b9c415a75b4b67bac11293
Rizki Prasetya <rizkiprass@gmail.com>
2023-01-03 16:09:37 +0700

    add kms

--- aws-kms.tf
+++ aws-kms.tf
@@ -7,3 +7,13 @@ module "kms-cwatch-flowlogs-kms" {
   region         = var.region
   key_policy     = true
 }
+
+module "cloudtrail-kms" {
+  source         = "./modules/aws-kms"
+  alias_name     = format("%s-%s-cloudtrail-logs", var.customer, var.environment)
+  description    = "KMS CMK for cloudtrail"
+  environment    = var.environment
+  product_domain = "cloudtrail"
+  region         = var.region
+  key_policy     = true
+}
