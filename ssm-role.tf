a1b771599e3f9430fe79951b8e87b52061a415c7
Rizki Prasetya <rizkiprass@gmail.com>
2023-01-04 19:09:20 +0700

    fix instance profile ec2 codeploy

--- ssm-role.tf
+++ ssm-role.tf
@@ -67,6 +67,14 @@ resource "aws_iam_instance_profile" "ssm-s3-profile" {
   role = aws_iam_role.ssm-s3-role.name
 }
 ############################### FOR CODEDEPLOY ##########################################
+#create policy permission to access s3 for download bundle
+resource "aws_iam_policy" "codedeploy-s3-ec2" {
+  name        = format("%s-codedeploy-ec2", var.project)
+  description = "attach this policy to ec2 instance profile for codedeploy purpose"
+  policy      = file("./template/codedeploy-instanceprofile.json")
+  tags        = local.common_tags
+}
+
 #1.Attach Policy SSMCore
 resource "aws_iam_role_policy_attachment" "ssmcore-attach-ssmcore3" {
   role       = aws_iam_role.ssmcore-codedeploy-role.name
@@ -79,18 +87,19 @@ resource "aws_iam_role_policy_attachment" "ssmcore-attach-cwatch3" {
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
 }
 
-#3.Attach Policy codedeploy-ec2
+#3. Attach Policy codedeploy-ec2
 resource "aws_iam_role_policy_attachment" "ssmcore-attach-codedeploy-ec2" {
   role       = aws_iam_role.ssmcore-codedeploy-role.name
-  policy_arn = aws_iam_policy.codedeploy-service-as.arn
+  policy_arn = aws_iam_policy.codedeploy-s3-ec2.arn
 }
-#Instance Profile
+
+#Instance Profile ec2 for codedeploy download bundled
 resource "aws_iam_instance_profile" "ssm-codedeploy-profile" {
   role = aws_iam_role.ssmcore-codedeploy-role.name
   name = format("%s-ssm-codedeploy-profile2", var.project)
 }
 
-#Create Role for ec2 contain 1.ssminstancecore, 2.cwagent, 3.codedeploy policy
+#Create Role for ec2 contain 1.ssminstancecore, 2.cwagent, 3.codedeploy policy. Please attach this role to ec2 that you want to use for codedeploy
 resource "aws_iam_role" "ssmcore-codedeploy-role" {
   name               = format("%s-ssmcore-codedeploy-role", var.project)
   assume_role_policy = file("template/assumepolicy.json")
