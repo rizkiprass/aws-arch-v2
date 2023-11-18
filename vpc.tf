f9b48afd67b8eb3dfd8f07661b2cc285fae19cfd
rizkiprass <rizkiprass@gmail.com>
2023-11-18 13:23:31 +0700

    add vpn

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,9 @@
+provider "aws" {
+  region     = var.aws_region
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.1.1"
@@ -8,7 +14,7 @@ module "vpc" {
   enable_dhcp_options              = true
   dhcp_options_domain_name_servers = ["AmazonProvidedDNS"]
   azs                              = ["${var.region}a", "${var.region}b"]
-  public_subnets                   = [var.Public_Subnet_AZA_1, var.Public_Subnet_AZB_2]
+  public_subnets                   = [var.Public_Subnet_AZA_1, var.Public_Subnet_AZB_1]
   private_subnets                  = [var.App_Subnet_AZA, var.App_Subnet_AZB]
   # intra_subnets                    = [var.Data_Subnet_AZ1, var.Data_Subnet_AZ2] //this is subnet only route to local vpc
   database_subnets = [var.Data_Subnet_AZA, var.Data_Subnet_AZB] // subnet db route to nat
@@ -22,6 +28,23 @@ module "vpc" {
   private_subnet_suffix = "private"
   intra_subnet_suffix   = "db"
   #  intra_subnet_suffix   = "data"
+
+  #Virtual Private Gateway
+  enable_vpn_gateway = true
+
+  customer_gateways = {
+    IP1 = {
+      bgp_asn     = 65000
+      ip_address  = "1.2.3.4"
+      device_name = "some_name"
+    },
+#    IP2 = {
+#      bgp_asn    = 65112
+#      ip_address = "5.6.7.8"
+#    }
+  }
+
+
   tags = local.common_tags
   #  # VPC Flow Logs (Cloudwatch log group and IAM role will be created)
   #  enable_flow_log                      = true
@@ -34,7 +57,7 @@ module "vpc" {
 
   #  //tags for vpc flow logs
   #  vpc_flow_log_tags = {
-  #    Name = format("%s-%s-vpc-flowlogs", var.customer, var.environment)
+  #    Name = format("%s-%s-vpc-flowlogs", var.project, var.environment)
   #  }
 }
 
@@ -63,82 +86,82 @@ resource "aws_eip" "eip-nat" {
 
 #
 
-//Create a db subnet with routing to nat
-resource "aws_subnet" "subnet-db-1a" {
-  vpc_id            = module.vpc.vpc_id
-  cidr_block        = var.Data_Subnet_AZA
-  availability_zone = format("%sa", var.aws_region)
-
-  tags = merge(local.common_tags,
-    {
-      Name = format("%s-%s-data-subnet-3a", var.customer, var.environment) //
-  })
-}
-
-resource "aws_subnet" "subnet-db-1b" {
-  vpc_id            = module.vpc.vpc_id
-  cidr_block        = var.Data_Subnet_AZB
-  availability_zone = format("%sb", var.aws_region)
-
-  tags = merge(local.common_tags,
-    {
-      Name = format("%s-%s-data-subnet-3b", var.customer, var.environment) //
-  })
-}
-
-resource "aws_route_table" "data-rt" {
-  vpc_id = module.vpc.vpc_id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = module.vpc.natgw_ids[0]
-  }
-
-  tags = merge(local.common_tags, {
-    Name = format("%s-%s-data-rt", var.customer, var.environment)
-  })
-}
-
-resource "aws_route_table_association" "rt-subnet-assoc-data-3a" {
-  subnet_id      = aws_subnet.subnet-db-1a.id
-  route_table_id = aws_route_table.data-rt.id
-}
-
-//Create a app subnet
-resource "aws_subnet" "subnet-app-1a" {
-  vpc_id            = module.vpc.vpc_id
-  cidr_block        = var.App_Subnet_AZA
-  availability_zone = format("%sa", var.aws_region)
-
-  tags = merge(local.common_tags,
-    {
-      Name = format("%s-%s-app-subnet-3a", var.customer, var.environment) //
-  })
-}
-
-resource "aws_subnet" "subnet-app-1b" {
-  vpc_id            = module.vpc.vpc_id
-  cidr_block        = var.App_Subnet_AZB
-  availability_zone = format("%sb", var.aws_region)
-
-  tags = merge(local.common_tags,
-    {
-      Name = format("%s-%s-app-subnet-3b", var.customer, var.environment) //
-  })
-}
-
-resource "aws_route_table" "app-rt" {
-  vpc_id = module.vpc.vpc_id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = module.vpc.natgw_ids[0]
-  }
-
-  tags = merge(local.common_tags, {
-    Name = format("%s-%s-app-rt", var.customer, var.environment)
-  })
-}
-
-resource "aws_route_table_association" "rt-subnet-assoc-app-3a" {
-  subnet_id      = aws_subnet.subnet-app-1a.id
-  route_table_id = aws_route_table.app-rt.id
-}
+#//Create a db subnet with routing to nat
+#resource "aws_subnet" "subnet-db-1a" {
+#  vpc_id            = module.vpc.vpc_id
+#  cidr_block        = var.Data_Subnet_AZA
+#  availability_zone = format("%sa", var.aws_region)
+#
+#  tags = merge(local.common_tags,
+#    {
+#      Name = format("%s-%s-data-subnet-3a", var.project, var.environment) //
+#  })
+#}
+#
+#resource "aws_subnet" "subnet-db-1b" {
+#  vpc_id            = module.vpc.vpc_id
+#  cidr_block        = var.Data_Subnet_AZB
+#  availability_zone = format("%sb", var.aws_region)
+#
+#  tags = merge(local.common_tags,
+#    {
+#      Name = format("%s-%s-data-subnet-3b", var.project, var.environment) //
+#  })
+#}
+#
+#resource "aws_route_table" "data-rt" {
+#  vpc_id = module.vpc.vpc_id
+#  route {
+#    cidr_block = "0.0.0.0/0"
+#    gateway_id = module.vpc.natgw_ids[0]
+#  }
+#
+#  tags = merge(local.common_tags, {
+#    Name = format("%s-%s-data-rt", var.project, var.environment)
+#  })
+#}
+#
+#resource "aws_route_table_association" "rt-subnet-assoc-data-3a" {
+#  subnet_id      = aws_subnet.subnet-db-1a.id
+#  route_table_id = aws_route_table.data-rt.id
+#}
+#
+#//Create a app subnet
+#resource "aws_subnet" "subnet-app-1a" {
+#  vpc_id            = module.vpc.vpc_id
+#  cidr_block        = var.App_Subnet_AZA
+#  availability_zone = format("%sa", var.aws_region)
+#
+#  tags = merge(local.common_tags,
+#    {
+#      Name = format("%s-%s-app-subnet-3a", var.project, var.environment) //
+#  })
+#}
+#
+#resource "aws_subnet" "subnet-app-1b" {
+#  vpc_id            = module.vpc.vpc_id
+#  cidr_block        = var.App_Subnet_AZB
+#  availability_zone = format("%sb", var.aws_region)
+#
+#  tags = merge(local.common_tags,
+#    {
+#      Name = format("%s-%s-app-subnet-3b", var.project, var.environment) //
+#  })
+#}
+#
+#resource "aws_route_table" "app-rt" {
+#  vpc_id = module.vpc.vpc_id
+#  route {
+#    cidr_block = "0.0.0.0/0"
+#    gateway_id = module.vpc.natgw_ids[0]
+#  }
+#
+#  tags = merge(local.common_tags, {
+#    Name = format("%s-%s-app-rt", var.project, var.environment)
+#  })
+#}
+#
+#resource "aws_route_table_association" "rt-subnet-assoc-app-3a" {
+#  subnet_id      = aws_subnet.subnet-app-1a.id
+#  route_table_id = aws_route_table.app-rt.id
+#}
